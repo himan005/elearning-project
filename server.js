46619ad6048d5993ea5860131a984102f060da78
himan005 <himanshugoej@gmail.com>
2019-07-09 15:22:24 +0530

    routing method is changed

--- server.js
+++ server.js
@@ -20,9 +20,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(morgan('dev'))
 
-app.get('/', (req, res, next)=>{
-    res.json("home")
-})
+require('./routes/main')(app)
 
 app.listen(secret.port, (err)=>{
     if(err){
