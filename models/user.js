dae462127f51e8ebdc86137b809bde6af4d4fdce
himan005 <himanshugoej@gmail.com>
2019-07-09 15:56:05 +0530

    Schema for user

---
+++ models/user.js
@@ -0,0 +1,30 @@
+let mongoose = require("mongoose")
+
+let UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true
+    },
+    
+    profile: {
+        name: {
+            type: String,
+            default: ''
+        },
+        picture:{
+            type: String,
+            default: ''
+        },
+    },
+    
+    coursesTeach: [{
+        course: { type: Schema.Types.ObjectId, ref: 'Course'}
+    }],
+
+    coursesTaken: [{
+        course: { type: Schema.Types.ObjectId, ref: 'Course'}
+    }],
+})
+
+module.exports = mongoose.model('User', UserSchema)
